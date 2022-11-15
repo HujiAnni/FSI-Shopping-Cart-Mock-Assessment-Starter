44711ec50eaaa4395f3f69f56c605c36f6a0496e
Jennifer Hu <jhu@LDI.INT>
2022-11-14 21:31:50 -0500

    add remove button function, show dynamic price

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,32 @@
 let quantityDisplay = document.querySelector(".total-quantity");
+let priceDisplay = document.querySelector(".total-price");
 let down = document.getElementById("quantity-down");
 let up = document.getElementById("quantity-up");
+let remove = document.querySelector(".remove");
+let cart = document.querySelector(".cart-item");
 
 let qty = 0;
+let item1Price = 15;
+let tot_price = 0;
 
 down.addEventListener('click',()=>{
     if (qty === 0) {
         qty = 0;
         quantityDisplay.textContent = `Quantity: ${qty}`;
+        priceDisplay.textContent = `Total Price: $${qty*item1Price}`;
     } else {
         qty -= 1;
-        quantityDisplay.innerHTML = `Quantity: ${qty}`;
+        quantityDisplay.textContent = `Quantity: ${qty}`;
+        priceDisplay.textContent = `Total Price: $${qty*item1Price}`;
     }
 });
 
 up.addEventListener('click',()=>{
     qty += 1;
-    quantityDisplay.innerHTML = `Quantity: ${qty}`;
+    quantityDisplay.textContent = `Quantity: ${qty}`;
+    priceDisplay.textContent = `Total Price: $${qty*item1Price}`;
 });
+
+remove.addEventListener('click',()=>{
+    cart.remove();
+})
