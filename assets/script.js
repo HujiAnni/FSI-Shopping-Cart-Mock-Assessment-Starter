6739e7d8296acd24b4b395a407a2844b020a0133
Jennifer Hu <jhu@LDI.INT>
2022-11-15 14:34:22 -0500

    add one more item in the cart

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,41 @@
-let quantityDisplay = document.querySelector(".total-quantity");
-let priceDisplay = document.querySelector(".total-price");
-let down = document.getElementById("quantity-down");
-let up = document.getElementById("quantity-up");
-let remove = document.querySelector(".remove");
-let cart = document.querySelector(".cart-item");
+let quantityDisplay = document.querySelectorAll(".total-quantity");
+let priceDisplay = document.querySelectorAll(".total-price");
+let down = document.querySelectorAll("#quantity-down");
+let up = document.querySelectorAll("#quantity-up");
+let removeItem1 = document.querySelector(".remove.item1");
+let cartItem1 = document.querySelector(".cart-item.item1");
+let removeItem2 = document.querySelector(".remove.item2");
+let cartItem2 = document.querySelector(".cart-item.item2");
 
-let qty = 0;
-let item1Price = 15;
+let qtys = [0, 0];
+let itemPrice = [15, 30];
 let tot_price = 0;
 
-down.addEventListener('click',()=>{
-    if (qty === 0) {
-        qty = 0;
-        quantityDisplay.textContent = `Quantity: ${qty}`;
-        priceDisplay.textContent = `Total Price: $${qty*item1Price}`;
-    } else {
-        qty -= 1;
-        quantityDisplay.textContent = `Quantity: ${qty}`;
-        priceDisplay.textContent = `Total Price: $${qty*item1Price}`;
-    }
-});
+for (let i = 0; i < itemPrice.length; i++){
+    down[i].addEventListener('click',()=>{
+        if (qtys[i] === 0) {
+            qtys[i] = 0;
+            quantityDisplay[i].textContent = `Quantity: ${qtys[i]}`;
+            priceDisplay[i].textContent = `Total Price: $${qtys[i]*itemPrice[i]}`;
+        } else {
+            qtys[i] -= 1;
+            quantityDisplay[i].textContent = `Quantity: ${qtys[i]}`;
+            priceDisplay[i].textContent = `Total Price: $${qtys[i]*itemPrice[i]}`;
+        }
+    });
+    
+    up[i].addEventListener('click',()=>{
+        qtys[i] += 1;
+        quantityDisplay[i].textContent = `Quantity: ${qtys[i]}`;
+        priceDisplay[i].textContent = `Total Price: $${qtys[i]*itemPrice[i]}`;
+    });
+}
 
-up.addEventListener('click',()=>{
-    qty += 1;
-    quantityDisplay.textContent = `Quantity: ${qty}`;
-    priceDisplay.textContent = `Total Price: $${qty*item1Price}`;
-});
 
-remove.addEventListener('click',()=>{
-    cart.remove();
+removeItem1.addEventListener('click',()=>{
+    cartItem1.remove();
+})
+
+removeItem2.addEventListener('click',()=>{
+    cartItem2.remove();
 })
